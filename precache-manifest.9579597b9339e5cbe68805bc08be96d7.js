e829ee1f727323064a47eab4ee128ac12dfa731a
rickychhoukdean <chhoukdean184@msn.com>
2020-06-12 02:21:03 -0400

    Updates

---
+++ precache-manifest.9579597b9339e5cbe68805bc08be96d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509be369379ff0b463060fceb1fc0e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "605b0a842d6e7eb17d91",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1cc429d78d5d2c3dbb4b",
+    "url": "/static/css/main.d99ac35c.chunk.css"
+  },
+  {
+    "revision": "605b0a842d6e7eb17d91",
+    "url": "/static/js/2.1cf17ebb.chunk.js"
+  },
+  {
+    "revision": "1cc429d78d5d2c3dbb4b",
+    "url": "/static/js/main.b2d9f284.chunk.js"
+  },
+  {
+    "revision": "9cfe4520ac0b909a4fd6",
+    "url": "/static/js/runtime~main.8eeb5630.js"
+  },
+  {
+    "revision": "fd5f2dca84f7141436c629bff96ae069",
+    "url": "/static/media/backgroundtestsmall-min.fd5f2dca.jpg"
+  },
+  {
+    "revision": "f19a740425945b6fc7be28667d8b5350",
+    "url": "/static/media/image2.f19a7404.jpg"
+  }
+]);
